3aefce7c09690c75c59dc0be78f7d2bc78695eec
l <lorenzo.regalzi@gmail.com>
2020-03-17 19:30:13 +0100

    Prima stilizzazione

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    var source= $('template').html();
+    var cardTemplate = Handlebars.compile(source)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
